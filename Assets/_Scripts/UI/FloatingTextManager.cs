5bcb14b3c6f9eec403983e67d666429d6fdadff6
SiddharthVarde22 <21stargames@gmail.com>
2025-06-19 23:26:32 +0530

    - Updated Floating Text manager (WIP)

--- Assets/_Scripts/UI/FloatingTextManager.cs
+++ Assets/_Scripts/UI/FloatingTextManager.cs
@@ -8,9 +8,30 @@ public class FloatingTextManager : MonoBehaviour
 
     ObjectPool<FloatingText> m_floatingTextPool;
     IScoreService m_scoreService;
+    const int MinimumFloatTextCount = 10;
 
     private void Start()
     {
         m_scoreService = ServiceLocator.GetService<IScoreService>(EServiceTypes.ScoreService);
+        m_floatingTextPool = new ObjectPool<FloatingText>(MinimumFloatTextCount, CreateNewFloatingText,
+            OnGetFloatingTextCalled, OnReleaseFloatingText, OnDestroyFloatingText);
+    }
+
+    private FloatingText CreateNewFloatingText()
+    {
+        FloatingText l_floatingText = Instantiate<FloatingText>(m_floatingTextPrefab, transform);
+        return l_floatingText;
+    }
+    private void OnGetFloatingTextCalled(FloatingText a_floatingText)
+    {
+
+    }
+    private void OnReleaseFloatingText(FloatingText a_floatingtext)
+    {
+
+    }
+    private void OnDestroyFloatingText(FloatingText a_floatingText)
+    {
+
     }
 }
